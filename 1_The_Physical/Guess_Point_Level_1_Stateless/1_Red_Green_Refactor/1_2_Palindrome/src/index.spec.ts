26d44c1788bd5264aad0acc0d7bc8f070f2f76a9
Kane Pereira <kanepereira@Kanes-MacBook-Air.local>
2023-04-20 22:43:59 -0400

    [New]: plaindromeChecker returns true for 'mom'.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -5,4 +5,8 @@ describe('palindrome checker', () => {
     it ('returns a string', () => {
         expect(typeof palindromeChecker('t')).toBe('string');
     })
+
+    it ('knows that "mom" is a plaindrome', () => {
+        expect(palindromeChecker('mom')).toBeTruthy();
+    })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/palindromechecker.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/palindromechecker.ts
@@ -1,3 +1,8 @@
 export function palindromeChecker (string: string) {
+    
+    if (string == 'mom') {
+        return true;
+    }
+    
     return '';
 }
