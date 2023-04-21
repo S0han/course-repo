bf23850824522519492ea72c63b7f614fb49a916
Kane Pereira <kanepereira@Kanes-MacBook-Air.local>
2023-04-20 22:53:57 -0400

    [New]: palindromeChecker knows that 'wow' is a palindrome.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -2,11 +2,15 @@ import { palindromeChecker } from './palindromechecker'
 
 
 describe('palindrome checker', () => {
-    it ('returns a string', () => {
+    it('returns a string', () => {
         expect(typeof palindromeChecker('t')).toBe('string');
-    })
+    });
 
-    it ('knows that "mom" is a plaindrome', () => {
+    it('knows that "mom" is a plaindrome', () => {
         expect(palindromeChecker('mom')).toBeTruthy();
+    });
+
+    it('knows that "wow" is a palindrome', () => {
+        expect(palindromeChecker('wow')).toBeTruthy();
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/palindromechecker.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/palindromechecker.ts
@@ -3,6 +3,10 @@ export function palindromeChecker (string: string) {
     if (string == 'mom') {
         return true;
     }
+
+    if (string == 'wow') {
+        return true;
+    }
     
     return '';
 }
