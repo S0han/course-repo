cf6a04defa1bd754a7e3b7237eb22b174691360d
Kane Pereira <kane.pereira18@gmail.com>
2023-05-14 03:17:02 -0400

    [New]: Fake result for the missing digit test

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,21 @@
+import { PasswordValidator } from '../src/passwordValidator';
+
 
 describe('password validator', () => {
+    it('returns "missing digit" for passwords without at least 1 digit', () => {
+        
+        let input = 'mom';
 
+        let response = new PasswordValidator();
+
+        expect(response.valPass().result).toBe(false);
+        expect(response.valPass().errorKey.length).toEqual(1);
+        expect(response.valPass().errorKey[0]).toEqual('Missing Digit');
+
+    })
 })
 
+//tried exporting namespace app and adding class to that export to get around error
+//tried breaking into functions but does not make sense as we need to return object
+//tried changing file name to get around omdule error
+//tried changing path
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/passwordValidator.ts
@@ -0,0 +1,15 @@
+export class PasswordValidator {
+
+    constructor() {
+        let result: boolean;
+        let errorKey = Array<String>;
+    }
+
+    valPass() {
+        return {
+            result: false,
+            errorKey: ['Missing Digit']
+        }
+    }
+
+}
