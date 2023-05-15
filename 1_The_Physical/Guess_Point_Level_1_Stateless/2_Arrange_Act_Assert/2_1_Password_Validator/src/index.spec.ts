0a79ff8d783cade987affb226d6fee83af62dd9d
Kane Pereira <kane.pereira18@gmail.com>
2023-05-14 21:47:45 -0400

    [new]: tests created to make sure the password is betwen 5 and 15 characters

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -4,7 +4,7 @@ import { PasswordValidator } from '../src/passwordValidator';
 describe('password validator', () => {
     it('returns "missing digit" for passwords without at least 1 digit', () => {
         
-        let password = 'mom';
+        let password = 'Momtaro';
 
         let response = new PasswordValidator();
 
@@ -12,7 +12,31 @@ describe('password validator', () => {
         expect(response.valPass(password).errorKey.length).toEqual(1);
         expect(response.valPass(password).errorKey[0]).toEqual('Missing Digit');
 
-    })
+    });
+
+    it('returns "Invalid Length - Password must be at least 5 characters"..', () => {
+
+        let password = 'Dad3';
+
+        let response = new PasswordValidator();
+
+        expect(response.valPass(password).result).toBe(false);
+        expect(response.valPass(password).errorKey.length).toEqual(1);
+        expect(response.valPass(password).errorKey[0]).toEqual('Invalid Length - Password must be at least than 5 characters');
+    });
+
+    it('returns "Invalid Length - Password can not exceed 15 characters"..', () => {
+
+        let password = 'Daddddddddddssdd3';
+
+        let response = new PasswordValidator();
+
+        expect(response.valPass(password).result).toBe(false);
+        expect(response.valPass(password).errorKey.length).toEqual(1);
+        expect(response.valPass(password).errorKey[0]).toEqual('Invalid Length - Password can not exceed 15 characters');
+    });
+
+
 })
 
 //tried exporting namespace app and adding class to that export to get around error
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/passwordValidator.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/passwordValidator.ts
@@ -11,7 +11,17 @@ export class PasswordValidator {
         
         if (!hasDigit(password)) {
             validated.errorKey.push('Missing Digit');
-            validated.result = false
+            validated.result = false;
+        }
+
+        if (fiveChar(password)) {
+            validated.errorKey.push('Invalid Length - Password must be at least than 5 characters');
+            validated.result = false;
+        }
+
+        if (fifteenChar(password)) {
+            validated.errorKey.push('Invalid Length - Password can not exceed 15 characters');
+            validated.result = false;
         }
         
         return validated;
@@ -21,4 +31,12 @@ export class PasswordValidator {
 
 function hasDigit(password: string): boolean {
     return /\d/.test(password);
+}
+
+function fiveChar(password: string): boolean {
+    return (password.length <= 5);
+}
+
+function fifteenChar(password: string): boolean {
+    return (password.length >= 15);
 }
