23c198bd294456defd8fae136504ed21b9790779
Kane Pereira <kane.pereira18@gmail.com>
2023-05-15 02:11:28 -0400

    [red]: fail test for missing digit

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,3 +1,13 @@
+import { passwordValidator } from './passwordValidator';
+
 describe('password validator', () => {
-   
+   it('returns "Missing Digit" for passwords without at least one digit', () => {
+        
+        let password = 'Momotaro';
+
+        expect(passwordValidator(password).result).toBe(false);
+        expect(passwordValidator(password).errorKey.length).toEqual(1);
+        expect(passwordValidator(password).errorKey[0]).toEqual('Missing Digit');
+
+   });
 })
