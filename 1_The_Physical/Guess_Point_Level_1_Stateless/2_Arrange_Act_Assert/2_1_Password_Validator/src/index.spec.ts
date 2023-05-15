308d05f5ead099995f2497f98e2612aca5f9a2fa
S0han <43890209+S0han@users.noreply.github.com>
2023-05-14 23:28:13 -0400

    Revert "Password validator may 14 1"

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -4,83 +4,15 @@ import { PasswordValidator } from '../src/passwordValidator';
 describe('password validator', () => {
     it('returns "missing digit" for passwords without at least 1 digit', () => {
         
-        let password = 'Momtaro';
+        let input = 'mom';
 
-        let response = new PasswordValidator();
+        let response = new PasswordValidator(input);
 
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(1);
-        expect(response.valPass(password).errorKey[0]).toEqual('Missing Digit');
+        expect(response.valPass().result).toBe(false);
+        expect(response.valPass().length).toEqual(1);
+        expect(response.valPass().errorKey[0]).toEqual('Missing Digit');
 
-    });
-
-    it('returns "Invalid Length - Password must be at least 5 characters"..', () => {
-
-        let password = 'Dad3';
-
-        let response = new PasswordValidator();
-
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(1);
-        expect(response.valPass(password).errorKey[0]).toEqual('Invalid Length - Password must be at least than 5 characters');
-    });
-
-    it('returns "Invalid Length - Password can not exceed 15 characters"..', () => {
-
-        let password = 'Daddddddddddssdd3';
-
-        let response = new PasswordValidator();
-
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(1);
-        expect(response.valPass(password).errorKey[0]).toEqual('Invalid Length - Password can not exceed 15 characters');
-    });
-
-    it('returns "Missing Upper-case Letter" for passwords without at least 1 upper-case letter', () => {
-        
-        let password = 'momtar999';
-
-        let response = new PasswordValidator();
-
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(1);
-        expect(response.valPass(password).errorKey[0]).toEqual('Missing Upper-case Letter');
-
-    });
-
-    it('returns both "Missing Digit" AND "Missing Upper-case Letter"..', () => {
-        
-        let password = 'momwww';
-        
-        let response = new PasswordValidator();
-
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(2);
-        expect(response.valPass(password).errorKey).toEqual(['Missing Digit', 'Missing Upper-case Letter']);
-    });
-
-    it('returns both "Missing Digit" AND "Missing Upper-case Letter" AND "Invalid Length - Password must be at least than 5 characters"..', () => {
-        
-        let password = 'mom';
-        
-        let response = new PasswordValidator();
-
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(3);
-        expect(response.valPass(password).errorKey).toEqual(['Missing Digit', 'Invalid Length - Password must be at least than 5 characters' , 'Missing Upper-case Letter', ]);
-    });
-
-    it('returns both "Missing Digit" AND "Missing Upper-case Letter" AND "Invalid Length - Password can not exceed 15 characters"..', () => {
-        
-        let password = 'mommmmmmmmmmmmmmm';
-        
-        let response = new PasswordValidator();
-
-        expect(response.valPass(password).result).toBe(false);
-        expect(response.valPass(password).errorKey.length).toEqual(3);
-        expect(response.valPass(password).errorKey).toEqual(['Missing Digit', 'Invalid Length - Password can not exceed 15 characters' , 'Missing Upper-case Letter', ]);
-    });
-
+    })
 })
 
 //tried exporting namespace app and adding class to that export to get around error
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/passwordValidator.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/passwordValidator.ts
@@ -1,52 +1,24 @@
 export class PasswordValidator {
+    password: string;
+    result: boolean;
+    errorKey: string[];
 
-    valPass(password: string)  {
-        let result: boolean = true;
-        const errorKey: string[] = [];
-        
-        const validated = {
-            result,
-            errorKey
-        }
-        
-        if (!hasDigit(password)) {
-            validated.errorKey.push('Missing Digit');
-        }
-
-        if (fiveChar(password)) {
-            validated.errorKey.push('Invalid Length - Password must be at least than 5 characters');
-        }
-
-        if (fifteenChar(password)) {
-            validated.errorKey.push('Invalid Length - Password can not exceed 15 characters');
-        }
-
-        if (!hasUpper(password)) {
-            validated.errorKey.push('Missing Upper-case Letter');
-        }
-
-        if (errorKey.length > 0) {
-            validated.result = false;
-        }
-        
-        console.log(validated);
-        return validated;
+    constructor(x: string) {
+        this.password = x;
+        this.result = false;
+        this.errorKey = ['Missing Digit'];
     }
 
-}
 
-function hasDigit(password: string): boolean {
-    return /\d/.test(password);
-}
+    valPass() {
 
-function fiveChar(password: string): boolean {
-    return (password.length <= 5);
-}
+        this.result = /\d/.test(this.password);
 
-function fifteenChar(password: string): boolean {
-    return (password.length >= 15);
-}
+        return {
+            result: this.result,
+            length: this.errorKey.length,
+            errorKey: this.errorKey,
+        }
+    }
 
-function hasUpper(password: string): boolean {
-    return /[A-Z]/.test(password);
 }
