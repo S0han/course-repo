95459ada5242f8b9845e8720b21aa351ea993775
Kane Pereira <kane.pereira18@gmail.com>
2023-05-15 02:32:42 -0400

    [new]: check to see if password length is at least 5 characters

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,6 +1,7 @@
 import { passwordValidator } from './passwordValidator';
 
 describe('password validator', () => {
+
    it('returns "Missing Digit" for passwords without at least one digit', () => {
         
         let password = 'Momotaro';
@@ -12,4 +13,18 @@ describe('password validator', () => {
         expect(response.errorKey[0]).toEqual('Missing Digit');
 
    });
+
+   it('returns "Invalid Length - Password must be at least 5 characters"..', () => {
+
+        let password = 'Dad3';
+
+        let response = passwordValidator(password);
+
+        expect(response.result).toBe(false);
+        expect(response.errorKey.length).toEqual(1);
+        expect(response.errorKey[0]).toEqual('Invalid Length - Password must be at least 5 characters');
+
+   });
+
+
 })
