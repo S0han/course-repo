4053f9976543ed300127e4e562a0f06c4bbad6ce
Kane Pereira <kane.pereira18@gmail.com>
2023-05-19 02:19:21 -0400

    [new]: fake it test to return a false output

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,16 @@
+import { milTime } from './milTime';
+
 
 describe('military time validator', () => {
 
+    it('will return "false" for an empty string', () => {
+
+        let timeInput = '';
+
+        const valMilTime = milTime(timeInput);
+
+        expect(valMilTime).toBe(false);
+
+    }); 
 
 })
---
+++ src/milTime.ts
@@ -0,0 +1,3 @@
+export function milTime(timeInput: string) {
+    return false
+}
