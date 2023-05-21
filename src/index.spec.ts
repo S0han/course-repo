f6d3585afe4fe91ed11fff9cd3270a14a482e8a4
Kane Pereira <kane.pereira18@gmail.com>
2023-05-20 22:43:22 -0400

    [refactor]: remove unnecessary variables for test cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,17 +4,13 @@ describe("military time validator", () => {
   it('will return "false" for an empty string', () => {
     const timeInput = "";
 
-    const valMilTime = milTime(timeInput);
-
-    expect(valMilTime).toBe(false);
+    expect(milTime(timeInput)).toBe(false);
   });
 
   it('must contain a " - " in the time range', () => {
     const timeInput = "10:21 -11:05";
 
-    const valMilTime = milTime(timeInput);
-
-    expect(valMilTime).toBe(false);
+    expect(milTime(timeInput)).toBe(false);
   });
 
     describe('can detect valid hours', () => {
